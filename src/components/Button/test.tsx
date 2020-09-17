ae26cc67be04ef1e45fbdd6a8d055e1a39951799
Erick Bernardo <erickvictor@gmail.com>
2020-09-16 20:31:08 -0400

    Add Button to support anchor/button types

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -45,9 +45,26 @@ describe('<Button />', () => {
   })
 
   it('should render an icon version', () => {
-    renderWithTheme(<Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>)
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />}>Buy now</Button>
+    )
 
     expect(screen.getByText(/Buy now/i)).toBeInTheDocument()
     expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('should render Button as a link', () => {
+    const { debug, container } = renderWithTheme(
+      <Button as="a" href="/link">
+        Buy now
+      </Button>
+    )
+
+    debug(container)
+
+    expect(screen.getByRole('link', { name: /buy now/i })).toHaveAttribute(
+      'href',
+      '/link'
+    )
   })
 })
