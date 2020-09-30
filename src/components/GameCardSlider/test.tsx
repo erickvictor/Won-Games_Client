642e18b19fc37ea1c4359830ee98c775d9de7313
Erick Bernardo <erickvictor@gmail.com>
2020-09-30 18:46:25 -0400

    Add GameCardSlider styles

--- src/components/GameCardSlider/test.tsx
+++ src/components/GameCardSlider/test.tsx
@@ -1,4 +1,5 @@
 import 'match-media-mock'
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCardSlider from '.'
@@ -31,6 +32,13 @@ const items = [
     img: 'https://source.unsplash.com/user/willianjusten/300x143',
     price: 'R$ 235,00',
     promotionalPrice: 'R$ 215,00'
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00'
   }
 ]
 
@@ -39,4 +47,16 @@ describe('<GameCardSlider />', () => {
     const { container } = renderWithTheme(<GameCardSlider items={items} />)
     expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
   })
+
+  it('should render white arrows if color passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  });
 })
