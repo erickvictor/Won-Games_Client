fd60595c8bb9d591bbf2a2237aa6fc0d44706acd
Erick Bernardo <erickvictor@gmail.com>
2020-10-16 17:51:07 -0400

    Add Home styles and Optimize Home tests

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,21 +1,35 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
 import Home from '.'
+
+const props = {
+  banners: bannersMock,
+  newGames: [gamesMock[0]],
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
+  upcommingHighlight: highlightMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
+  freeHighligth: highlightMock
+}
 
 describe('<Home />', () => {
   it('should render menu and footer', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(
-      screen.getByRole('heading', { name: /contact us/i })
+      screen.getByRole('heading', { name: /follow us/i })
     ).toBeInTheDocument()
-  })
-
-  it('should render the sections', () => {
-    renderWithTheme(<Home />)
-
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+    
     expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
       screen.getByRole('heading', { name: /most popular/i })
@@ -26,5 +40,9 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
+
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+    expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
 })
