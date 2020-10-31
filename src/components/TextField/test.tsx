3996f9390755227c90ea1d7b7734e722acaecffe
Erick Bernardo <erickvictor@gmail.com>
2020-10-30 22:10:45 -0400

    Add option to icon on TextField

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -1,28 +1,35 @@
 import { screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { Email } from '@styled-icons/material-outlined'
 
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import TextField from '.'
 
 describe('<TextField />', () => {
-  it('Render with Label', () => {
+  it('Renders with Label', () => {
     renderWithTheme(<TextField label="Label" labelFor="Field" id="Field" />)
 
     expect(screen.getByLabelText('Label')).toBeInTheDocument()
   })
 
-  it('Render without Label', () => {
+  it('Renders without Label', () => {
     renderWithTheme(<TextField />)
 
     expect(screen.queryByLabelText('Label')).not.toBeInTheDocument()
   })
 
-  it('Render with placeholder', () => {
+  it('Renders with placeholder', () => {
     renderWithTheme(<TextField placeholder="hey you" />)
 
     expect(screen.getByPlaceholderText('hey you')).toBeInTheDocument()
   })
+
+  it('Renders with Icon', () => {
+    renderWithTheme(<TextField icon={<Email data-testid="icon" />} />)
+
+    expect(screen.getByTestId('icon')).toBeInTheDocument()
+  });
 
   it('Changes its value when typing', async () => {
     const onInput = jest.fn()
