e66f3834d1cb2992f9898c59f4f425ef521906d5
Erick Bernardo <erickvictor@gmail.com>
2020-11-01 16:29:00 -0400

    Add FormSignIn tests

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -28,7 +28,7 @@ const FormSignIn = () => (
       </Button>
 
       <S.FormLink>
-        Don't have an account?{' '}
+        Don’t have an account?{' '}
         <Link href="/sign-up">
           <a>Sign up</a>
         </Link>
