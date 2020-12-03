918864e0968717cde4ce4fda38fa02ca979c1a8b
Erick Bernardo <erickvictor@gmail.com>
2020-12-02 20:03:25 -0400

    Add GameInfo styles, mock and story

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -11,7 +11,7 @@ const props = {
 
 describe('<GameInfo />', () => {
   it('should render game informations', () => {
-    renderWithTheme(<GameInfo {...props} />)
+    const { container } = renderWithTheme(<GameInfo {...props} />)
 
     // esperar por um heading (title)
     expect(
@@ -22,6 +22,8 @@ describe('<GameInfo />', () => {
     // expect(screen.getByText(/\$210,00/)).toBeInTheDocument()
     // esperar pelo price
     expect(screen.getByText(/game description/i)).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render buttons', () => {
