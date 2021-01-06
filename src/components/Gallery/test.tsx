6f9ff0bf95583b339b869deac3b04c2bc2a5c697
Erick Bernardo <erickvictor@gmail.com>
2021-01-06 14:39:17 -0400

    Add close with ESC Modal on Gallery

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -22,7 +22,7 @@ describe('<Gallery />', () => {
   it('should handle open modal', () => {
     renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
 
-    // selecionar o nosso MenuFull
+    // selecionar o nosso modal
     const modal = screen.getByLabelText('modal')
 
     // verificar se o modal tá escondido
@@ -40,7 +40,7 @@ describe('<Gallery />', () => {
   it('should handle close modal when overlay or button clicked', () => {
     renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
 
-    // selecionar o nosso MenuFull
+    // selecionar o nosso modal
     const modal = screen.getByLabelText('modal')
 
     // clicar no botão de abrir o menu e verificar se ele abriu
@@ -50,6 +50,24 @@ describe('<Gallery />', () => {
 
     //clicar para fechar o modal
     fireEvent.click(screen.getByRole('button', { name: /close modal/i }))
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+  })
+
+  it('should handle close modal when ESC button is pressed', () => {
+    const { container } = renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    // selecionar o nosso moadl
+    const modal = screen.getByLabelText('modal')
+
+    // clicar no botão de abrir o menu e verificar se ele abriu
+    fireEvent.click(
+      screen.getByRole('button', { name: /Thumb - Gallery Image 1/i })
+    )
+
+    //clicar para fechar o modal
+    fireEvent.keyUp(container, { key: 'Escape' })
+
     expect(modal.getAttribute('aria-hidden')).toBe('true')
     expect(modal).toHaveStyle({ opacity: 0 })
   })
