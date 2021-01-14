a2a81f2b462b60f2b6a22e875d30f9e992ad17bd
Erick Bernardo <erickvictor@gmail.com>
2021-01-13 20:25:00 -0400

    Add GameDetails to page and fix publisher on GameDetails

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -39,10 +39,19 @@ export async function getStaticProps() {
       gameInfo: {
         title: 'Cyberpunk 2077',
         price: '59.00',
-        description: 'Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality'
+        description:
+          'Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification. You play as V, a mercenary outlaw going after a one-of-a-kind implant that is the key to immortality'
       },
       gallery: galleryMock,
-      description: descriptionHTML
+      description: descriptionHTML,
+      details: {
+        developer: 'CD PROJECT RED',
+        releaseDate: '2020-11-21T23:00:00',
+        platforms: ['windows'],
+        publisher: 'CD PROJECT RED',
+        rating: 'BR18',
+        genres: ['Action', 'Role-playing']
+      }
     }
   }
 }
