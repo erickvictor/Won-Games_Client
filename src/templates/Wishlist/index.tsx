b880ca224c4630627f85ec6bd4ec9550fb1a6fb8
Erick Bernardo <erickvictor@gmail.com>
2021-01-14 21:19:02 -0400

    Add Grid to Wishlist

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -2,11 +2,13 @@ import Base from 'templates/Base'
 
 import Heading from 'components/Heading'
 import Showcase from 'components/Showcase'
-import { Container } from 'components/Container'
 import GameCard, { GameCardProps } from 'components/GameCard'
+import { Container } from 'components/Container'
+import { Grid } from 'components/Grid'
 import { HighlightProps } from 'components/Highlight'
 
 import * as S from './styles'
+
 
 export type WishlistTemplateProps = {
   games?: GameCardProps[]
@@ -25,9 +27,11 @@ const Wishlist = ({
         Wishlist
       </Heading>
 
-      {games?.map((game, index) => (
-        <GameCard key={`wishlist-${index}`} {...game} />
-      ))}
+      <Grid>
+        {games?.map((game, index) => (
+          <GameCard key={`wishlist-${index}`} {...game} />
+        ))}
+      </Grid>
     </Container>
     <Showcase
       title="You may like these games"
