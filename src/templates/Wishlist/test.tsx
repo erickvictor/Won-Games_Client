5c0437907164a7cda720261c0a1c9e5e558cb5a2
Erick Bernardo <erickvictor@gmail.com>
2021-01-14 20:52:23 -0400

    Add Wishlist initial tests

--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -1,13 +1,30 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Wishlist from '.'
 
+import gamesMock from 'components/GameCardSlider/mock'
+import highlichtMock from 'components/Highlight/mock'
+
+const props = {
+  recommendedHighlight: highlichtMock,
+  recommendedGames: gamesMock
+}
+
+jest.mock('components/Showcase', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock Showcase" />
+  }
+}))
+
 describe('<Wishlist />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Wishlist />)
+    renderWithTheme(<Wishlist {...props} />)
 
-    expect(screen.getByRole('heading', { name: /Wishlist/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /wishlist/i })
+    ).toBeInTheDocument()
+    expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
   })
 })
