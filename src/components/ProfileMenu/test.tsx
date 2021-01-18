49dda2fa1559764281f6d168dc469b3f47ed173d
Erick Bernardo <erickvictor@gmail.com>
2021-01-18 14:28:38 -0400

    Add ProfileMenu activeLink

--- src/components/ProfileMenu/test.tsx
+++ src/components/ProfileMenu/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import theme from 'styles/theme'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import ProfileMenu from '.'
@@ -17,4 +18,13 @@ describe('<ProfileMenu />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the menu with an active link defined', () => {
+    renderWithTheme(<ProfileMenu activeLink="/profile/cards" />)
+
+    expect(screen.getByRole('link', { name: /my cards/i })).toHaveStyle({
+      background: theme.colors.primary,
+      color: theme.colors.white
+    })
+  });
 })
