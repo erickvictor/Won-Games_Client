5653d53fc7bff61b21cf30240e13c66a626e0bb9
Erick Bernardo <erickvictor@gmail.com>
2021-01-18 18:43:35 -0400

    Add OrdersList

--- src/templates/Profile/test.tsx
+++ src/templates/Profile/test.tsx
@@ -1,12 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Profile from '.'
 
 describe('<Profile />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Profile />)
-
-    expect(screen.getByRole('heading', { name: /Profile/i })).toBeInTheDocument()
 
   })
 })
