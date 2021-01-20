8a5f48613522e771b4c76db3d80afe1b03eb65d7
Erick Bernardo <erickvictor@gmail.com>
2021-01-20 18:27:55 -0400

    Update CartList to have hasButton option

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -6,11 +6,21 @@ import mockItems from './mock'
 
 describe('<CartList />', () => {
   it('should render the cart list', () => {
-    const {container} = renderWithTheme(<CartList items={mockItems} total="R$ 330,00" />)
+    const { container } = renderWithTheme(
+      <CartList items={mockItems} total="R$ 330,00" />
+    )
 
     expect(screen.getAllByRole('heading')).toHaveLength(2)
-    expect(screen.getByText('R$ 330,00')).toHaveStyle({ color: '#F231A5'})
+    expect(screen.getByText('R$ 330,00')).toHaveStyle({ color: '#F231A5' })
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the button', () => {
+    renderWithTheme(
+      <CartList items={mockItems} total="R$ 330,00" hasButton />
+    )
+
+    expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
   })
 })
