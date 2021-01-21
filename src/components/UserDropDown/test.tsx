43d67fddc346a0b421c4e31a9c9f6846212a9020
Erick Bernardo <erickvictor@gmail.com>
2021-01-21 14:34:44 -0400

    Add UserDropdown structure and styles

---
+++ src/components/UserDropDown/test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+
+import UserDropDown from '.'
+
+describe('<UserDropDown />', () => {
+  it('should render the username', () => {})
+
+  it('should render the menu', () => {})
+})
