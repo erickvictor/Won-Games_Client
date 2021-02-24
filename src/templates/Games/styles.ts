7fce2aa1f73883e829cedd7bc3c7a96e887665a6
Erick Bernardo <erickvictor@gmail.com>
2021-02-23 20:29:44 -0400

    Add loading to ShowMore

--- src/templates/Games/styles.ts
+++ src/templates/Games/styles.ts
@@ -2,7 +2,6 @@ import styled, { css } from 'styled-components'
 import { Container } from 'components/Container'
 import media from 'styled-media-query'
 
-
 export const Main = styled(Container)`
   ${({ theme }) => css`
     ${media.greaterThan('medium')`
@@ -14,15 +13,24 @@ export const Main = styled(Container)`
 `
 
 export const ShowMore = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 10rem;
+`
+
+export const ShowMoreButton = styled.div`
   ${({ theme }) => css`
-    color: ${theme.colors.white};
     text-align: center;
-    padding: ${theme.spacings.medium};
     text-transform: uppercase;
     font-weight: bold;
     cursor: pointer;
+    color: ${theme.colors.white};
     > svg {
       color: ${theme.colors.primary};
     }
   `}
+`
+export const ShowMoreLoading = styled.img`
+  width: 4rem;
 `
