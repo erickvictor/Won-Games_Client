67f0df0d9437a2f7ca44c4b0a40f7d40ac5aec05
Erick Bernardo <erickvictor@gmail.com>
2021-03-16 20:19:46 -0400

    Final adjusts to typescript and mocks

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -7,7 +7,6 @@ import CartList, { CartListProps } from 'components/CartList'
 import Heading from 'components/Heading'
 import Showcase from 'components/Showcase'
 import Base from 'templates/Base'
-import Empty from 'components/Empty'
 
 import * as S from './styles'
 
@@ -22,8 +21,6 @@ const Cart = ({
   recommendedTitle,
   recommendedGames,
   recommendedHighlight,
-  items,
-  total,
   cards
 }: CartProps) => {
   const handlePayment = () => ({})
@@ -35,19 +32,11 @@ const Cart = ({
           My cart
         </Heading>
 
-        {items?.length ? (
           <S.Content>
-            <CartList items={items} total={total} />
+            <CartList />
 
             <PaymentOptions cards={cards} handlePayment={handlePayment} />
           </S.Content>
-        ) : (
-          <Empty
-            title="Your cart is empty"
-            description="Go back to the store and explore great games and offers"
-            hasLink
-          />
-        )}
 
         <Divider />
       </Container>
