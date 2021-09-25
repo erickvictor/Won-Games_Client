f032bcc6b73460e3ffe24906c4da9c80eccf50f6
Erick Bernardo <erickvictor@gmail.com>
2021-09-24 20:13:01 -0400

    Change signOut to redirect to home instead of the previous page

--- src/components/UserDropDown/index.tsx
+++ src/components/UserDropDown/index.tsx
@@ -1,49 +1,63 @@
+import Link from 'next/link'
+import { signOut } from 'next-auth/client'
+import { useRouter } from 'next/router'
 import {
   AccountCircle,
   FavoriteBorder,
   ExitToApp
 } from '@styled-icons/material-outlined'
 import { ChevronDown } from '@styled-icons/boxicons-regular'
-import { signOut } from 'next-auth/client'
-import Link from 'next/link'
+
 import Dropdown from 'components/Dropdown'
+
 import * as S from './styles'
 
 export type UserDropdownProps = {
   username: string
 }
 
-const UserDropDown = ({ username }: UserDropdownProps) => (
-  <Dropdown
-    title={
-      <>
-        <AccountCircle size={24} />
-        <S.Username>{username}</S.Username>
-        <ChevronDown size={24} />
-      </>
-    }
-  >
-    <S.Nav>
-      <Link href="/profile/me" passHref>
-        <S.Link>
-          <AccountCircle />
-          <span>My profile</span>
-        </S.Link>
-      </Link>
+const UserDropDown = ({ username }: UserDropdownProps) => {
+  const { push } = useRouter()
 
-      <Link href="/wishlist" passHref>
-        <S.Link>
-          <FavoriteBorder />
-          <span>Wishlist</span>
-        </S.Link>
-      </Link>
+  return (
+    <Dropdown
+      title={
+        <>
+          <AccountCircle size={24} />
+          <S.Username>{username}</S.Username>
+          <ChevronDown size={24} />
+        </>
+      }
+    >
+      <S.Nav>
+        <Link href="/profile/me" passHref>
+          <S.Link>
+            <AccountCircle />
+            <span>My profile</span>
+          </S.Link>
+        </Link>
 
-      <S.Link role="button" onClick={() => signOut()} title="Sign out">
-        <ExitToApp />
-        <span>Sign out</span>
-      </S.Link>
-    </S.Nav>
-  </Dropdown>
-)
+        <Link href="/wishlist" passHref>
+          <S.Link>
+            <FavoriteBorder />
+            <span>Wishlist</span>
+          </S.Link>
+        </Link>
+
+        <S.Link
+          role="button"
+          onClick={async () => {
+            const data = await signOut({ redirect: false, callbackUrl: '/' })
+            push(data.url)
+          }}
+          title="Sign out"
+        >
+          <ExitToApp />
+          <span>Sign out</span>
+        </S.Link>
+      </S.Nav>
+    </Dropdown>
+  )
+}
 
 export default UserDropDown
