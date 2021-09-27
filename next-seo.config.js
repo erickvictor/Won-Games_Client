f1ef2cb588b359490e21190ad4021250ec5ab01c
Erick Bernardo <erickvictor@gmail.com>
2021-09-27 18:36:44 -0400

    Add NextSEO default

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://wongames.vercelapp.com',
+    site_name: 'Won Games',
+    title: 'Won Games'
+  },
+  twitter: {
+    handle: '@erickvictor',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
