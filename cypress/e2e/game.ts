704353d6be6479a7fd2abe7e83767a83c5bf4fd1
Erick Bernardo <erickvictor@gmail.com>
2022-10-03 19:13:45 -0400

    Start e2e tests to Game page

---
+++ cypress/e2e/game.ts
@@ -0,0 +1,17 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Game Page', () => {
+  it('should render game page sections', () => {
+    cy.visit('/game/hellblade-senuas-sacrifice-pack')
+
+    cy.getByDataCy("game-info").within(() => {
+      cy.findByRole('heading', { name: /Hellblade: Senua's Sacrifice/i }).should('exist')
+      cy.findByText(/Includes the VR Edition of the critically acclaimed BAFTA/i).should('exist')
+      cy.findByText('$14.00').should('exist')
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
+    })
+
+    // gallery
+    cy.findAllByRole('button', { name: /thumb \-/i }).should('have.length.gt', 0)
+  });
+});
